32f4f7c5e9b7036a54ac5bce5325c86aa6b9ee4f
lpazienza <l.pazienza1@studenti.uniba.it>
2016-11-29 19:38:46 +0100

    Exercise completed

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,58 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+		int size = numbersStr.length();
+		if (size == 0) {
+			return 0;
+		} else {
+			if (numbersStr.startsWith("-")) {
+				throw new StringCalculatorException();
+			} else {
+				String newString = "";
+				if (numbersStr.contains("//")) {
+					String delimetersString = numbersStr.substring(2).split("\n")[0];
+					newString = numbersStr.split("\n")[1];
+					String delimeters[] = computeDelimeters(delimetersString);
+					for (int i = 0; i < delimeters.length; i++) {
+						newString = newString.replace(delimeters[i], ",");
+					}
+				} else {
+					newString = numbersStr.replaceAll("\n", ",");
+				}
+				return getSum(newString);
+			}
+		}
+
+	}
+
+	public int getSum(String expression) throws StringCalculatorException {
+		if (expression.contains(",,")) {
+			throw new StringCalculatorException();
+		} else {
+			int sum = 0;
+			String[] numbers = expression.split(",");
+			int operand = 0;
+			for (int i = 0; i < numbers.length; i++) {
+				operand = Integer.parseInt(numbers[i]);
+				if (operand > 1000) {
+					operand = 0;
+				}
+				sum += operand;
+			}
+			return sum;
+		}
+	}
+
+	public String[] computeDelimeters(String stringWithDelimeters) {
+		String[] holder = stringWithDelimeters.split("\\[");
+		String holderToghether = "";
+		for (int i = 0; i < holder.length; i++) {
+			holderToghether += holder[i];
+		}
+		String[] delimeters = holderToghether.split("\\]");
+		return delimeters;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -2,13 +2,136 @@ package it.uniba.tdd.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class StringCalculatorTest {
+import it.uniba.tdd.*;
 
+public class StringCalculatorTest {
+	 StringCalculator firstSC;
+	 int result;
+	 
+	 @Before
+	public void setUp(){
+		firstSC = new StringCalculator();
+		result = 0;
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void emptyStringReturnsSum0() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("");
+		//Assert
+		assertEquals(0,result);
+	}
+	
+	@Test
+	public void StringReturnsTheOperandWithOneInput() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("2");
+		//Assert
+		assertEquals(2+0,result);
+	}
+	
+	@Test
+	public void StringReturnsTheSumOfThe2NumbersInInput() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("2,5");
+		//Assert
+		assertEquals(2+5,result);
+	}
+	
+	@Test
+	public void StringReturnsTheSumOfAllNumbersInInput() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("2,5,5,0,2,3,51,9");
+		//Assert
+		assertEquals(2+5+5+0+2+3+51+9,result);
+	}
+	
+	@Test
+	public void StringReturnsTheSumOfAllNumbersInInputWithNewLineAsDelimiter() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("1\n2,3");
+		//Assert
+		assertEquals(1+2+3,result);
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void throwsExceptionWithDelimitersInSequence() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("1,\n");
 	}
 
+	@Test(expected = StringCalculatorException.class)
+	public void throwsExceptionWithCustomDelimitersInSequence() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//;\n1;3;;5");
+	}
+	
+	@Test
+	public void StringReturnsTheSumOfAllNumbersInInputWithCustomDelimiter() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//del\n2del3del4del7");
+		//Assert
+		assertEquals(2+3+4+7,result);
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void throwsExceptionWithNegativeNumber() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("-56");
+	}
+	
+	@Test
+	public void numbersBiggerThen1000AreIgnored() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("2,1001");
+		//Assert
+		assertEquals(2+0,result);
+	}
+	
+	@Test
+	public void delimiterCanBeOfAnyLength() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//***\n1***2***3");
+		//Assert
+		assertEquals(1+2+3,result);
+	}
+	
+	@Test
+	public void CalculateSumWithMultipleDelimeters() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//[%][del]\n1%2del3");
+		//Assert
+		assertEquals(1+2+3,result);
+	}
+	
+	@Test
+	public void CalculateSumWithMultipleDelimetersLength() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//[%][del][asderta]\n1%2del3asderta78asderta7");
+		//Assert
+		assertEquals(1+2+3+78+7,result);
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void ThrowsExceptionWithCustomDelimeterRepeated() throws StringCalculatorException{
+		//Arrange in setUp
+		//Act
+		result = firstSC.add("//[%][del][asderta]\n1%2del3asdertaasderta");
+	}
 }
